3ecb2a2b928bb9b7e1645919679d36cb08b24674
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-03-27 01:59:37 -0500

    finished the for loop section.

---
+++ array_p1/array_p1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  array_p1
+//
+//  Created by Daniel A. Noventa on 3/27/15.
+//  Copyright (c) 2015 Noventa Foundation. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
--- for_loops_p1/for_loops_p1/main.swift
+++ for_loops_p1/for_loops_p1/main.swift
@@ -8,5 +8,63 @@
 
 import Foundation
 
-println("Hello, World!")
+var aNumber:Int = 0
+var counter = 0
 
+do{
+    ++aNumber
+    ++counter
+}
+while aNumber <= 8
+
+println(counter)
+
+/*
+while aNumber <= 3 {
+    ++aNumber
+    println(aNumber)
+}*/
+
+/*
+var topNumber = 3
+for index in 0...topNumber{
+    println(index)
+}*/
+
+/*
+for index in 0...6{
+    
+    println("\(index) is the first number and \(index * 5) is \(index) times 5")
+    
+}
+*/
+
+/*
+for character in "Mammoth" {
+    println(" \(character) ")
+}
+*/
+/*
+for var i = 0; i < 3; ++i{
+    println(i)
+}
+*/
+/*
+for character in "Mammoth"{
+    println("\(character) is the letter in mammoth")
+}
+*/
+/*
+for var i = 0; i < 4; ++i{
+    println("We have \(i) apples")
+}
+
+*/
+/*
+println("Start!")
+
+for var index = 1; index < 4; ++index{
+    println(index)
+}
+println("exit")
+*/
