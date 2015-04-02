fcd038c3dc8193779ee87b0266b4c51ae58d240b
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-01 20:40:03 -0500

    teaching about floats and types with arrays. Pretty basic stuff.

--- array_p1/array_p1/main.swift
+++ array_p1/array_p1/main.swift
@@ -8,7 +8,14 @@
 
 import Foundation
 
-var testArray = ["John", "Don", "Sally", "Pete"]
-var anotehrArray = [1, 5, 6, 7]
+var testArray = ["John", "Don", "Sally", "Pete", "Katie", "Lynda"]
+var anotherArray = [1, 5, 6, 7]
+var floatArray = [0.1, 1.5, 4.6]
 
-println(testArray)
+let countArray1 = testArray.count
+let countArray2 = anotherArray.count
+let countArray3 = floatArray.count
+
+println(countArray1)
+println(countArray2)
+println(countArray3)
