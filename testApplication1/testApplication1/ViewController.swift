c08622b894de8eaf2c75aae7442adfcc6d9a0214
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-06 21:01:22 -0500

    Just created first iphone app skeleton.

---
+++ testApplication1/testApplication1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testApplication1
+//
+//  Created by Daniel A. Noventa on 4/6/15.
+//  Copyright (c) 2015 Noventa Foundation. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
