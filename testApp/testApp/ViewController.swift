09c19ad36ec73e0036050e96031ca87da4c220fb
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-14 21:29:58 -0500

    New app practice.

---
+++ testApp/testApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testApp
+//
+//  Created by Daniel A. Noventa on 4/14/15.
+//  Copyright (c) 2015 Noventa Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
