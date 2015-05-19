8393830cc1eefea804139e03072085603e842d94
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-19 16:56:33 -0500

    Arrays, and Dictionaries.

--- LyndaSwift_001/LyndaSwift_001/main.swift
+++ LyndaSwift_001/LyndaSwift_001/main.swift
@@ -8,6 +8,39 @@
 
 import Foundation
 
+
+
+
+var states = ["AYE" : "Aye Aye Capn", "Oy" : "O-My-G", "BR" : "Im cold!"]
+
+println(states["AYE"])
+states["FL"] = "Florida"
+states.updateValue("Nevada", forKey: "NV")
+println(states)
+
+
+let daysInMagic = [1, 4, 5, 7, 8, 9, 23, 252, 25, 12 ,345, 7, 23, 654]
+var colorStuff = ["Bread", "Chicken", "Uber", "Almonds"]
+
+var flavaFlav = ["good", "bad"]
+
+var num = flavaFlav.count
+println("there are \(num) falves")
+for days in daysInMagic{
+    println("\(days)")
+}
+for colors in colorStuff {
+    println("\(colors)")
+}
+for flav in flavaFlav {
+    println("flavas in thing \(flav) count is \(num)")
+    num--
+}
+
+
+
+
+
 let city = "Caprica"
 var day = "Tuesday"
 var temp = 75
@@ -36,3 +69,21 @@ default:
     println("blah")
     break
 }
+
+func myFunction() {
+    println("This function has been called")
+}
+
+myFunction()
+
+func nameFunction(name : String = "Jannet Doe", age : Int) {
+    println("My Name is \(name) and I'm \(age) years old")
+}
+
+nameFunction(name:"dan", 90)
+
+func nameMagic(name : String) -> String {
+    return name
+}
+var namo = nameMagic("Fred")
+println("my name is \(namo)")
