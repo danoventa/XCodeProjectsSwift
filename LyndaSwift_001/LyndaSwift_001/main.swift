bc51f5c28e3c2215cda0f4f9c293817fac7f0280
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-19 15:08:56 -0500

    Adding some practice form lynda sites.

---
+++ LyndaSwift_001/LyndaSwift_001/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LyndaSwift_001
+//
+//  Created by Daniel A. Noventa on 5/19/15.
+//  Copyright (c) 2015 Noventa Foundation. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
