38bfeeb6c953ad64d328975cc5e3e82b99c0beaa
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-19 16:25:55 -0500

    Switch statements have ranges. Also, in line string entrepolation.:

--- LyndaSwift_001/LyndaSwift_001/main.swift
+++ LyndaSwift_001/LyndaSwift_001/main.swift
@@ -12,4 +12,27 @@ let city = "Caprica"
 var day = "Tuesday"
 var temp = 75
 
-println("The hihg for \(city) on \(day) is \(temp)")
+println("The hihg for \(city) on \(day) is \(temp) degrees")
+
+var price = 20.55
+var quantity = 75
+
+println("the stuff is \(price * Double(quantity))")
+
+if temp > quantity {
+    println("it's hot")
+} else {
+    println("blah blah blah")
+}
+
+switch temp {
+case 0...75:
+    println("magic")
+    break
+case 76...100:
+    println("other magics")
+    break
+default:
+    println("blah")
+    break
+}
